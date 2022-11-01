9cd4a66b9f6afdb617a909952443c5457c527ad2
zdavid <zdavid@i-on.net>
2022-11-01 10:23:04 +0900

    CodeMirror 폰트 설정.

--- jql/tutorial/src/views/About.vue
+++ jql/tutorial/src/views/About.vue
@@ -2,8 +2,18 @@
   <form>
     <div class="mb-3">
       <label for="exampleFormControlInput1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      <Codemirror id="code-editor"
+          v-model:value="code"
+          :options="cmOptions"
+          border
+          placeholder="test placeholder"
+          :height="200"
+          @change="change"
+      />
     </div>
+    <b-button @click="execute">
+      run
+    </b-button>
     <div class="mb-3">
       <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
       <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
@@ -12,7 +22,45 @@
 </template>
 
 <script>
-export default {}
+import Codemirror from "codemirror-editor-vue3";
+
+// language
+import "codemirror/mode/javascript/javascript.js";
+
+// theme
+import "codemirror/theme/dracula.css";
+
+import { ref } from "vue";
+export default {
+  components: { Codemirror },
+  setup() {
+    const code = ref(`
+var i = 0;
+for (; i < 9; i++) {
+  console.log(i);
+  // more statements
+}`);
+
+    return {
+      code,
+      cmOptions: {
+        mode: "text/javascript", // Language mode
+        theme: "dracula", // Theme
+        lineNumbers: true, // Show line number
+        smartIndent: true, // Smart indent
+        indentUnit: 4, // The smart indent unit is 2 spaces in length
+        foldGutter: true, // Code folding
+        styleActiveLine: true, // Display the style of the selected row
+      },
+    };
+  },
+  methods : {
+    execute() {
+      console.log(this.code)
+    }
+  }
+};
+
 </script>
 
 <style>
@@ -20,4 +68,9 @@ export default {}
     padding-top: 2em;
     padding-right: 2em;
   }
+  .CodeMirror * {
+    font-family: Curier, monospace;
+    font-size: small;
+  }
+
 </style>
