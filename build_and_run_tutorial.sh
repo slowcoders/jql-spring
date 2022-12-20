c897fca687e26bff6194a25c255d3b207194306f
zeedhoon <zeedhoon@gmail.com>
2022-12-20 10:44:33 +0900

    JDK 17 support

--- build_and_run_tutorial.sh
+++ build_and_run_tutorial.sh
@@ -1,8 +1,4 @@
-pushd basic-sample-app/tutorial
-npm install
-npm run build
-popd
-pushd basic-sample-app
+pushd tutorial+sample-app
 sh ./db/start_postgres.sh
 ./gradlew bootRun --console=plain
 popd
