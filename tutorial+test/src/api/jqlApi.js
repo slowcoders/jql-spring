b8d2cd1076e3b5708fcb94d898d4025a06eba5dd
zeedhoon <>
2023-03-10 16:33:22 +0900

    Jpa reference field mapping 오류 수정. JoinConstraint 추가.

--- tutorial+test/src/api/jqlApi.js
+++ tutorial+test/src/api/jqlApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:7007/api/jql/starwars/character';
+const baseUrl = 'http://localhost:7007/api/jql/starwars_jpa/character';
 
 const default_options = {
     select: "*",
